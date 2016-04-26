4d13a900e4df5cf866074fa69cce41344c3066e8
COL Nuschi <sbarraza@nuschi.co>
2016-04-26 10:26:52 -0500

    menu
    se cambio el menu a php

--- guardar.php
+++ guardar.php
@@ -2,13 +2,13 @@
 	$nom=$_POST["nom"];
 	$ape=$_POST["ape"];
 	$tdoc=$_POST["tdoc"];
-	$ndoc=$_POST["ndoc"];
-	$dir=$_POST["dir"];
+	$doc=$_POST["doc"];
+	$dirr=$_POST["dirr"];
 	$barr=$_POST["barr"];
 	$ciu=$_POST["ciu"];
 	$tel=$_POST["tel"];
 	$cor=$_POST["cor"];
 	$db=mysql_connect("localhost","root","");
 	mysql_select_db("nuschi",$db);
-	mysql_query("insert into acudiente values('$nom','$ape','$tdoc','$ndoc','$dir', '$barr','$ciu','$tel','$cor')");
+	mysql_query("insert into acudiente values('$nom','$ape','$tdoc','$doc','$dir', '$barr','$ciu','$tel','$cor')");
 ?>

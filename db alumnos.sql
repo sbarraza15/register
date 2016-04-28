c16b177721a58bbd86f6eaa6eb42818cf20f22ae
COL Nuschi <sbarraza@nuschi.co>
2016-04-28 11:19:20 -0500

    Added files via upload

---
+++ db alumnos.sql
@@ -0,0 +1,18 @@
+use nuschi;
+create table alumnos(
+Nombre varchar(50),
+Apellido varchar(50),
+Identificacion bigint,
+GradoCursado bigint,
+GradoAspirante bigint,
+Edad bigint,
+Sexo varchar(10),
+Direccion bigint,
+Barrio varchar(20),
+Ciudad varchar(20),
+Telefono bigint,
+Correo varchar(20),
+primary key(Identificacion,Nombre));
+select * from alumnos
+drop table alumnos;
+show tables;
